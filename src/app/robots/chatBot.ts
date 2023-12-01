f45f4c43968396b07fa331c76431d5b59087602d
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-12-01 09:31:49 -0300

    fix:case

--- src/app/robots/chatBot.ts
+++ src/app/robots/chatBot.ts
@@ -40,7 +40,7 @@ class ChatBoot {
     const result = await service.findOrCreate(number, name);
 
     console.log(wamid);
-    if (input.length > 200 || (number == "557781032674" && wamid)) {
+    if (input.length > 200 && number != "557781032674") {
       sendMessage(
         number,
         "Ops seu texto é muito grande!. Só são permitido 200 caracteres.",
